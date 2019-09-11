3547e65f4f8710d6580a2d04ce08ce2ff738adb3
David Moles <dmoles@berkeley.edu>
2019-09-11 13:12:34 -0700

    Get MediaElement.js and basic route working

--- app/controllers/player_controller.rb
+++ app/controllers/player_controller.rb
@@ -2,12 +2,20 @@ class PlayerController < ApplicationController
   TIND_ID_PARAMS = %w[901m 901o].freeze
 
   def index
-    @collection = params[:collection]
-    @files = split_files(params[:files])
-    @tind_ids = tind_ids(params)
+    @collection = player_params[:collection]
+    @files = split_files(player_params[:files])
+    @tind_ids = tind_ids(player_params)
   end
 
   private
+
+  def player_params
+    @player_params ||= begin
+      # :format is a default parameter added from routes.rb
+      permitted = [:collection, :files, :format] + TIND_ID_PARAMS
+      params.permit(*permitted)
+    end
+  end
 
   # @param files_param a semicolon-delimited (%3B-delimited in the URL) list of files
   def split_files(files_param)
@@ -17,8 +25,7 @@ class PlayerController < ApplicationController
   end
 
   def tind_ids(params)
-    tind_params = params.permit(*TIND_ID_PARAMS)
-    tind_params.to_h.find_all do |k, _|
+    params.to_h.find_all do |k, _|
       k.to_s =~ /^[0-9]{3}[a-z]$/
     end.to_h
   end
