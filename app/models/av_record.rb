bf06ec426de539222d18dc2edcb33e71e514b3d2
David Moles <dmoles@berkeley.edu>
2019-10-16 09:55:08 -0700

    Simplify partials

--- app/models/av_record.rb
+++ app/models/av_record.rb
@@ -5,6 +5,8 @@ require 'metadata/record'
 class AvRecord
   attr_reader :files, :metadata_key, :metadata_record
 
+  delegate(:title, :description, :fields, :restrictions, to: :metadata_record)
+
   # Initializes a new AV record.
   #
   # @param files [Array<AvFile>] The AV files
@@ -13,20 +15,6 @@ class AvRecord
     @files = files
     @metadata_record = Metadata::Record.find(metadata_key)
     @metadata_key = metadata_key
-  end
-
-  # Gets the title of this record.
-  #
-  # @return [String] The title.
-  def title
-    metadata_record.title
-  end
-
-  # Gets the access restrictions for this record.
-  #
-  # @return [Restrictions] The restrictions.
-  def restrictions
-    metadata_record.restrictions
   end
 
   # @return [Boolean] True if public, false otherwise
