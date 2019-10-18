1e5af09ff3c99e72ea1c8a79f88f6c3b6797f43c
David Moles <dmoles@berkeley.edu>
2019-10-18 13:29:08 -0700

    Set Content-Security-Policy to allow insecure AV, possibly

--- config/application.rb
+++ config/application.rb
@@ -57,5 +57,24 @@ module AvPlayer
     config.video_base_url = ENV.fetch('LIT_VIDEO_BASE_URL') do
       'http://www.lib.berkeley.edu/videosecret/'
     end
+
+    # ############################################################
+    # Content security
+
+    config.content_security_policy do |policy|
+      wowza_base = URI.parse(config.wowza_base_url)
+      wowza_src = URI::HTTP.build(scheme: wowza_base.scheme, host: wowza_base.host, port: wowza_base.port)
+
+      video_base = URI.parse(config.video_base_url)
+      video_src = URI::HTTP.build(scheme: video_base.scheme, host: video_base.host, port: video_base.port)
+
+      policy.default_src(:self)
+      policy.style_src(:self, 'https://cdn.jsdelivr.net', 'https://fonts.googleapis.com')
+      policy.script_src(:self, 'https://cdn.jsdelivr.net')
+      policy.font_src(:self, 'https://fonts.gstatic.com')
+      policy.img_src(:self, 'https://cdn.jsdelivr.net', 'data:')
+      policy.media_src(wowza_src.to_s, video_src.to_s, 'blob:')
+      policy.connect_src(wowza_src.to_s, video_src.to_s)
+    end
   end
 end
