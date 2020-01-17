8fde7b46823fff35a5c140db17d79aba42688cc9
David Moles <dmoles@berkeley.edu>
2020-01-17 11:55:23 -0800

    Handle broken track URLs more gracefully

--- lib/av_player/track_extensions.rb
+++ lib/av_player/track_extensions.rb
@@ -37,10 +37,14 @@ module AV
 
       def mp4_path(relative_path)
         URI.join(video_base_uri, relative_path)
+      rescue URI::InvalidURIError => e
+        log_invalid_uri(relative_path, e)
       end
 
       def mp3_path(collection, relative_path)
         URI.join(wowza_base_uri, "#{collection}/mp3:#{relative_path}/playlist.m3u8")
+      rescue URI::InvalidURIError => e
+        log_invalid_uri(relative_path, e)
       end
 
       def wowza_base_uri
@@ -49,6 +53,15 @@ module AV
 
       def video_base_uri
         Rails.application.config.video_base_uri
+      end
+
+      def log_invalid_uri(relative_path, e)
+        message = "Error parsing relative path #{relative_path.inspect}"
+        message << "#{e.class} (#{e.message}):\n"
+        message << "  " << e.backtrace.join("\n  ")
+        Rails.logger.warn(message)
+
+        nil
       end
     end
 
