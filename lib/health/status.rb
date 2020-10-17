f09d6422a0643400a5cd4eb41e02ebffa97182e3
David Moles <dmoles@berkeley.edu>
2020-10-16 17:00:24 -0700

    Update to Rails 6.0.3.4

--- lib/health/status.rb
+++ lib/health/status.rb
@@ -4,7 +4,13 @@ module Health
   # Enumerated list of health states
   class Status < TypesafeEnum::Base
     # NOTE: states should be ordered from least to most severe
+
+    # OK
     new :PASS, 200
+
+    # Failure should probably be 503 Service Unavailable, but Zenoss only distinguishes between
+    # response classes (2xx/3xx/4xx/5xx). To get Zenoss to report health check failures differently
+    # from 500 Internal Server Error etc., we use 429 Too Many Requests.
     new :WARN, 429
 
     # Concatenates health states, returning the more severe state.
