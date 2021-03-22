dbb6c5468eff8bc80e8ab731a34a4e7edbb35e86
David Moles <dmoles@berkeley.edu>
2021-03-22 15:18:39 -0700

    Add specs for BuildInfo

---
+++ spec/initializers/build_info_spec.rb
@@ -0,0 +1,54 @@
+require 'rails_helper'
+
+module AvPlayer
+  describe BuildInfo do
+    describe :as_html_comment do
+      let(:comment_re) { /<!--(.*)-->/m }
+      let(:build_info) { AvPlayer::BuildInfo.build_info }
+      let(:test_info) do
+        {
+          CI: 'true',
+          BUILD_TIMESTAMP: '2021-03-22T21:57:07+0000',
+          BUILD_URL: 'https://jenkins.lib.berkeley.edu/job/gitlab/job/lap/job/lap%252Favplayer/job/LIT-2266-build-args/1/',
+          DOCKER_TAG: 'containers.lib.berkeley.edu/lap/avplayer/lit-2266-build-args:build-1',
+          GIT_BRANCH: 'LIT-2266-build-args',
+          GIT_COMMIT: 'e2493452b7e6ebe98856fa3aff6b2c8f24063586',
+          GIT_URL: 'git@git.lib.berkeley.edu:lap/avplayer.git'
+        }.freeze
+      end
+
+      before(:each) do
+        @info_orig = build_info.instance_variable_get(:@info)
+        @comment_orig = build_info.instance_variable_get(:@html_comment)
+
+        build_info.instance_variable_set(:@info, test_info)
+        build_info.instance_variable_set(:@html_comment, nil)
+      end
+
+      after(:each) do
+        build_info.instance_variable_set(:@info, @info_orig)
+        build_info.instance_variable_set(:@html_comment, @comment_orig)
+      end
+
+      it 'returns an HTML comment' do
+        expect(BuildInfo.as_html_comment).to match(comment_re)
+      end
+
+      it 'includes all build info' do
+        test_info.each do |k, v|
+          expect(BuildInfo.as_html_comment).to include("#{k}: #{v}")
+        end
+      end
+
+      it 'escapes multiple dashes' do
+        doubled = test_info.transform_values { |v| v.gsub('-', '--') }
+        build_info.instance_variable_set(:@info, doubled)
+
+        test_info.each do |k, v|
+          expected_value = v.gsub('-', '&#45;&#45;')
+          expect(BuildInfo.as_html_comment).to include("#{k}: #{expected_value}")
+        end
+      end
+    end
+  end
+end
--- spec/request/player_request_spec.rb
+++ spec/request/player_request_spec.rb
@@ -2,14 +2,16 @@ require 'rails_helper'
 require 'health/check'
 
 describe PlayerController, type: :request do
+  before(:each) do
+    AV::Config.wowza_base_uri = 'https://wowza.example.edu/'
+  end
+
   describe :health do
     it 'returns the health check result' do
       expected_status = 200
       expected_json = {
         'status' => 'pass',
-        'details' => Health::Check.all_checks.keys.map do |c|
-          [c, 'pass']
-        end.to_h
+        'details' => Health::Check.all_checks.keys.map { |c| [c, 'pass'] }.to_h
       }
 
       check = instance_double(Health::Check)
@@ -22,6 +24,19 @@ describe PlayerController, type: :request do
       expect(response).to have_http_status(:ok)
 
       expect(JSON.parse(response.body)).to eq(expected_json)
+    end
+  end
+
+  describe :index do
+    it 'includes build information in comments' do
+      search_url = 'http://oskicat.berkeley.edu/search~S1?/.b23305522/.b23305522/1%2C1%2C1%2CB/marc~b23305522'
+      stub_request(:get, search_url).to_return(status: 200, body: File.read('spec/data/b23305522.html'))
+      manifest_url = 'https://wowza.example.edu/Pacifica/mp3:PRA_NHPRC1_AZ1084_00_000_00.mp3/playlist.m3u8'
+      stub_request(:head, manifest_url).to_return(status: 200)
+
+      get '/Pacifica/b23305522'
+
+      expect(response.body).to include(AvPlayer::BuildInfo.as_html_comment)
     end
   end
 end
