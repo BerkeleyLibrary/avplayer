e63fee9158207611bdc474ef9ef8a7212db0a569
David Moles <dmoles@berkeley.edu>
2023-02-21 12:02:23 -0800

    Log app settings as well as AV config

--- config/application.rb
+++ config/application.rb
@@ -11,11 +11,34 @@ Bundler.require(*Rails.groups)
 module AvPlayer
   class Application < Rails::Application
 
+    APP_SETTINGS_KEYS = %i[campus_networks_uri show_homepage allow_preview cas_host].freeze
+
+    # ############################################################
+    # RAILS DEFAULTS
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
     # ############################################################
-    # External services
+    # Application settings
+
+    # Campus networks URL
+    # Note that this includes LBNL, and that's intentional
+    config.campus_networks_uri = ENV.fetch('CAMPUS_NETWORKS_URL', 'https://framework.lib.berkeley.edu/campus-networks/')
+
+    # Display home page (defaults to false)
+    config.show_homepage = ENV.fetch('LIT_SHOW_HOMEPAGE', false)
+
+    # Allow direct track previews (defaults to false)
+    config.allow_preview = ENV.fetch('LIT_ALLOW_PREVIEW', false)
+
+    # TODO: configure this more elegantly and make it play better with Selenium tests
+    config.cas_host = ENV.fetch('CAS_HOST') do
+      "auth#{'-test' unless Rails.env.production?}.berkeley.edu"
+    end
+
+    # ############################################################
+    # AV::Config settings
 
     # Search URL for TIND metadata
     config.tind_base_uri = ENV.fetch('LIT_TIND_BASE_URL', 'https://digicoll.lib.berkeley.edu/') # production
@@ -41,23 +64,15 @@ module AvPlayer
     # Alma view state key to use when generating Alma permalinks
     config.alma_permalink_key = ENV.fetch('LIT_ALMA_PERMALINK_KEY', 'iqob43')
 
-    # Campus networks URL
-    # Note that this includes LBNL, and that's intentional
-    config.campus_networks_uri = ENV.fetch('CAMPUS_NETWORKS_URL', 'https://framework.lib.berkeley.edu/campus-networks/')
-
-    # Display home page (defaults to false)
-    config.show_homepage = ENV.fetch('LIT_SHOW_HOMEPAGE', false)
-
-    # Allow direct track previews (defaults to false)
-    config.allow_preview = ENV.fetch('LIT_ALLOW_PREVIEW', false)
-
-    # TODO: configure this more elegantly and make it play better with Selenium tests
-    config.cas_host = ENV.fetch('CAS_HOST') do
-      "auth#{'-test' unless Rails.env.production?}.berkeley.edu"
-    end
+    # ############################################################
+    # Log configuration
 
     config.after_initialize do
       AvPlayer::BuildInfo.log_to(Rails.logger)
+
+      app_settings = APP_SETTINGS_KEYS.to_h { |attr| [attr, Rails.application.config.send(attr)] }
+      Rails.logger.info("#{AvPlayer::Application} settings:", settings: app_settings)
+
       BerkeleyLibrary::AV::Config.log_settings!(to_logger: Rails.logger)
     rescue StandardError => e
       Rails.logger.error('Error logging build & config info', e)
