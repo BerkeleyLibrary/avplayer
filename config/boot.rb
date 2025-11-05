8773e5427efc30cad3a0d2a23b92fa33b64001c0
Daniel Schmidt <danschmidt5189@berkeley.edu>
2025-11-04 17:04:54 -0800

    AP-483: Use avcore 0.4.3 to use TIND API for metadata

--- config/boot.rb
+++ config/boot.rb
@@ -1,3 +1,7 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
+
+# Load Docker secrets from /run/secrets into ENV
+require 'berkeley_library/docker'
+BerkeleyLibrary::Docker::Secret.load_secrets!
